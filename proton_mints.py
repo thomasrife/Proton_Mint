359c1e9ae72e465ae9f4c6dcdcb2b5133b2ef883
thomi.rife <thomi.rife@gmail.com>
2022-08-03 09:37:08 -0300

    Mejoras para el formulario de la Query

--- proton_mints.py
+++ proton_mints.py
@@ -1,8 +1,21 @@
+from unittest import result
 import requests
 import pandas as pd
 import json
 
-url = "https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=11860010&toBlock=15210766&address=0x63174FA9680C674a5580f7d747832B2a2133Ad8f&topic0=0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef&&page=1&offset=1000&apikey=WGNWEA21FGPSPTHKGIG7A3IGBIXFBHJV2N"
+FromBlock = input("From Block: ")
+ToBlock = input("To Block: ")
+Address = input("Address: ")
+Topic = input("Topic: ")
+Page = input("Page: ")
+Offset = input("Offset: ")
+ApiKey = input("ApiKey: ")
+
+def url_maker(fromBlock, toBlock, address, topic, page, offset, apiKey):
+    url_query = f"https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock={fromBlock}&toBlock={toBlock}&address={address}&topic0={topic}&topic0_1_opr=and&topic1=0x0000000000000000000000000000000000000000000000000000000000000000&page={page}&offset={offset}&apikey={apiKey}"
+    return url_query
+
+url = url_maker(FromBlock, ToBlock, Address, Topic, Page, Offset, ApiKey)
 
 headers = {
     'Accept': 'application/json',
